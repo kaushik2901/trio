a77e50e6f9b5c6960a42560b844edaf612fde253
root <kaushikjadav602@gmail.com>
2018-10-25 00:21:26 +0530

    Updates

---
+++ precache-manifest.c8cf1fa4dc3c7575f01ff7d4a0a99c29.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "012062ef2de0fc2de889c151bab40138",
+    "url": "/trio/static/media/group.012062ef.svg"
+  },
+  {
+    "revision": "c76919eaa73e3768d58f",
+    "url": "/trio/static/css/main.49f1f4b5.chunk.css"
+  },
+  {
+    "revision": "b22fe888d7b7b2016003",
+    "url": "/trio/static/js/1.b22fe888.chunk.js"
+  },
+  {
+    "revision": "ddf48801e26871408490",
+    "url": "/trio/static/js/runtime~main.ddf48801.js"
+  },
+  {
+    "revision": "a8dffdbfc22217a47b1237b4bf3852b6",
+    "url": "/trio/static/media/logo.a8dffdbf.gif"
+  },
+  {
+    "revision": "1cf8b6415c129ba0867ae48cbb7cf074",
+    "url": "/trio/static/media/training.1cf8b641.svg"
+  },
+  {
+    "revision": "ae8d36be4f78a57253179991b16e8fc1",
+    "url": "/trio/static/media/house.ae8d36be.svg"
+  },
+  {
+    "revision": "c76919eaa73e3768d58f",
+    "url": "/trio/static/js/main.c76919ea.chunk.js"
+  },
+  {
+    "revision": "c0cd138271276bb33a87c2d174015455",
+    "url": "/trio/static/media/facebook.c0cd1382.svg"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/trio/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/trio/static/media/linkedin.4b475fb3.svg"
+  },
+  {
+    "revision": "a6372485318244c18a497ae9a9b62467",
+    "url": "/trio/static/media/youtube.a6372485.svg"
+  },
+  {
+    "revision": "182d5ef4d173a3cd93897242a6fd56a5",
+    "url": "/trio/static/media/twitter.182d5ef4.svg"
+  },
+  {
+    "revision": "a48708803a2693aa810c20fcf94604d5",
+    "url": "/trio/index.html"
+  }
+];
